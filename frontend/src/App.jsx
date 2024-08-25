cadd1496546de9a7b164538b59b5a46363afc8e2
Selim BAYINDIR <byndr28@gmail.com>
2024-08-25 14:17:36 +0300

    Components Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+
+
+import './App.css'
+import Categories from './components/Categories/Categories'
+import { Footer } from './components/Layout/Footer/Footer'
+import { Header } from './components/Layout/Header/Header'
+import Policy from './components/Layout/Policy/Policy'
+import Sliders from './components/Slider/Sliders'
+
+
+function App() {
+  return (
+    <>
+    <div>
+    <Header/>
+    <Sliders/>
+    <Categories />
+    <Policy/>
+    <Footer/>
+
+    </div>
+    </>
+  )
+}
+
+export default App
