fead528e2798d85043499562e26ffb1bb0172b2b
Selim BAYINDIR <byndr28@gmail.com>
2024-08-25 15:27:46 +0300

    CampaignsStarter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 
 
-import './App.css'
-import Categories from './components/Categories/Categories'
-import { Footer } from './components/Layout/Footer/Footer'
-import { Header } from './components/Layout/Header/Header'
-import Policy from './components/Layout/Policy/Policy'
-import Sliders from './components/Slider/Sliders'
-
-
+import Categories from './components/Categories/Categories';
+import { Footer } from './components/Layout/Footer/Footer';
+import { Header } from './components/Layout/Header/Header';
+import Policy from './components/Layout/Policy/Policy';
+import Products from './components/Products/Products';
+import Sliders from './components/Slider/Sliders';
+import './App.css';
 function App() {
   return (
     <>
@@ -15,6 +14,7 @@ function App() {
     <Header/>
     <Sliders/>
     <Categories />
+    <Products/>
     <Policy/>
     <Footer/>
 
